cb06cc7f122fb0756dcfa6e250a7376cadf6b44d
Eduardo S. Brito - archlinux laptop <edusoubrit09@gmail.com>
2026-04-27 23:59:30 -0300

    Adiciona estrutura de produtos

---
+++ src/models/cart.py
@@ -0,0 +1,8 @@
+# Import
+
+# Class
+class UserCart:
+    def __init__(self,cpf,product,quantity):
+        self.user_cpf = cpf
+        self.product = product
+        self. quantity = quantity
---
+++ src/models/product.py
@@ -0,0 +1,6 @@
+class Product:
+    def __init__(self,name,product_id,price,stock):
+        self.name = name
+        self.product_id = product_id
+        self.price = price
+        self.stock = stock
