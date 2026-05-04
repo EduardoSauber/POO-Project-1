435c63b7c5c32e4e04ee86ea2d19d71d327edf79
Eduardo S. Brito - archlinux laptop <edusoubrit09@gmail.com>
2026-05-04 18:36:46 -0300

    Altera linha de código 'read' e '__write'

--- src/controller/productController.py
+++ src/controller/productController.py
@@ -1,5 +1,6 @@
 # Import
 import json
+from src.models import cart
 from src.models.product import Product
 from src.models.cart import UserCart
 
@@ -12,14 +13,18 @@ class CartManager:
         self.__all_carts = []
 
     def read(self):
+        '''
         try:
             with open(f"src/controller/data/user_carts.json", 'r') as FILE:
                 usr_cart = json.load(FILE)
                 self.__all_carts = [UserCart(**cart) for cart in usr_cart]
         except FileNotFoundError:
             print("Não existem carrinhos registrados.")
+        '''
+        pass
 
     def __write(self):
+        '''
         try:
             with open('src/controllers/data/user_carts.json', 'w') as FILE:
                 usr_cart = [vars(usr_cart) for usr_cart in self.__all_carts]
@@ -27,12 +32,17 @@ class CartManager:
                 print("Arquivo 'user_carts.json' gravado com exito.")
         except FileNotFoundError:
             raise TypeError("O sistema não conseguiu gerar o arquivo 'user_carts.json'.")
+        '''
+        pass
+
+    def createCart(self,cpf):
+        new_cart = UserCart(cpf,None)
+        self.__all_carts.append(new_cart)
+        self.__write()
+        return new_cart
 
     def addToCart(self,cpf,product,quantity=1):
-        new_cart_item = UserCart(cpf,product,quantity)
-        self.__all_carts.append(new_cart_item)
-        self.__write()
-        return new_cart_item
+        pass
 
     def getUserCart(self,cpf):
         return [item for item in self.__all_carts if item.cpf == cpf]
