446f0b414cc54c5bc2fa0e53c4f5509ca3c6e9e0
Eduardo S. Brito - archlinux <edusoubrit09@gmail.com>
2026-05-31 02:57:00 -0300

    Reorganiza ViewRouter.py.

--- src/view/ViewRouter.py
+++ src/view/ViewRouter.py
@@ -1,112 +1,41 @@
-import getpass
-
 from src.controller.appController import AppManager
+from view.authView import AuthView
+from view.userView import UserView
+from view.adminView import AdminView
 
-class Terminal_View:
-    def __init__(self):
-        self._app_controller = AppManager()
+class ViewRouter:
+    def __init__(self,file_path):
+        self._app = AppManager(file_path)
 
-    @property
-    def personal_session_id(self):
-        return self._app_controller.personal_session_id
+        self.running = True
+
+        self._auth_view = AuthView(self._app)
+        self._user_view = UserView(self._app)
+        self._admin_view = AdminView(self._app)
 
     def start(self):
-        while True:
-            if self.personal_session_id is None:
-                print("Bem vindo!\nO que deseja realizar?\n")
-                print("[ 1 ] - Log-in")
-                print("[ 2 ] - Sign-in")
-                print("[ 0 ] - Sair")
-                opcao = input("Opção: ")
-
-                match opcao:
-                    case '1':
-                        self.login_page()
-                    case '2':
-                        self.signin_page()
-                    case '0':
-                        print("Saindo do sistema.")
-                        break
-                    case _:
-                        print("Opção inválida!")
+        while self.running:
+            if not self._app.personal_session_id:
+                case = self._auth_view.main_menu()
+                if case == "STOP":
+                    self.stop()
             else:
-                #user = self._user_manager.get_session_id_user(self.personal_session_id)
-                user = self._app_controller.is_user_admin
-                if user:
-                    self.admin_main_menu()
-                else:
-                    self.user_main_menu()
+                admin = self._app.is_user_admin
+                view = self._user_view if not admin else self._admin_view
+                case = view.main_menu()
+                if case == "STOP":
+                    self.stop()
+                elif case == "LOGOUT":
+                    self._app.logout()
 
-    def login_page(self):
-        while self.personal_session_id is None:
-            print("--- LOGAR ---")
-            try:
-                user_id = input("Digite seu CPF: ")
-                user_password = getpass.getpass("Digite sua senha: ")
-                self._app_controller.login(user_id, user_password)
-            except ValueError as error:
-                print(f"Erro: {error}. Tente novamente.")
-
-    def signin_page(self):
-        while self.personal_session_id is None:
-            print("--- CADASTRAR-SE ---")
-            try:
-                pass
-            except ValueError as error:
-                print(f"Erro: {error}. Tente novamente.")
-
-
-    def user_main_menu(self):
-        print("LOGOU")
-
-    def admin_main_menu(self):
-        print("--- Painel de Administração ---")
-
-        perms = [] #self._user_manager.get_session_permissions(self.personal_session_id)
-
-        print("[ 1 ] - Ver Catálogo de Produtos")
-        print("[ 2 ] - Ver Relatório Diário")
-        if 'cashier' in perms or 'admin' in perms:
-            print("-- Operações de Caixa --")
-            print("[ 2 ] - Processar Venda Física")
-            print("[ 3 ] - [W.I.P]")
-        if 'admin' in perms:
-            print("-- Gerenciamento de Sistema --")
-            print("[ 4 ] - Gerenciar Clientes")
-            print("[ 5 ] - Gerenciar Estoque")
-        print("-- Sistema --")
-        print("[ 0 ] - Sair")
-
-        opcao = input("Opção: ")
-        match opcao:
-            case '0':
-                print("Fazendo logout.")
-                #self._user_manager.logout(self.personal_session_id)
-                #self.personal_session_id = None
-                return
-            case '2':
-                pass
-            case '3':
-                pass
-            case '4':
-                if 'admin' in perms:
-                    pass
-                    #self.client_manager()
-                else:
-                    print("Opção inválida!")
-            case '5':
-                if 'admin' in perms:
-                    pass
-                    #self.stock_manager()
-                else:
-                    print("Opção inválida!")
-            case _:
-                print("Opção inválida!")
+    def stop(self):
+        print("Saindo do sistema...")
+        self.running = False
 
 # testbench
 def testbench():
     print("Rodando modo teste:")
-    app = Terminal_View()
+    app = ViewRouter("../controller/data")
     app.start()
 if __name__ == '__main__':
     testbench()
