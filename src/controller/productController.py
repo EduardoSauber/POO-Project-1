788d9e70425038888a93400e4337c6a2aabb5b52
Eduardo S. Brito - archlinux <edusoubrit09@gmail.com>
2026-05-31 04:50:13 -0300

    Adiciona leitura e escrita em ProductManager.

--- src/controller/productController.py
+++ src/controller/productController.py
@@ -8,14 +8,28 @@ from src.models.cart import UserCart
 ########################################################################################################################
 # Class
 class ProductManager:
-    def __init__(self):
-        self.__all_products= [] # recebe class Product
+    def __init__(self,data_path):
+        self.__all_products= []
+        self.__DATA_PATH = data_path
+
+        self.read()
 
     def read(self):
-        pass
+        try:
+            with open(f"{self.__DATA_PATH}/products.json", "r") as FILE:
+                prdct_data = json.load(FILE)
+                self.__all_products = [Product(**data) for data in prdct_data]
+        except FileNotFoundError:
+            print("productController: Não foi encontrado um banco de dados de produtos.")
 
     def __write(self):
-        pass
+        try:
+            with open(f"{self.__DATA_PATH}/products.json", "w") as FILE:
+                prdct_data = [product.to_dict() for product in self.__all_products]
+                json.dump(prdct_data, FILE)
+                print(f"productController: Aquivo 'products.json' gravado com exito.")
+        except FileNotFoundError:
+            raise TypeError(f"productController: O sistema não conseguiu gerar o arquivo 'products.json'.")
 
     def add_to_stock(self,product_id:str,product_name:str,price:float,quantity_to_stock:int):
         if product_id is not None:
@@ -26,7 +40,7 @@ class ProductManager:
             else:
                 novo_produto = Product(product_name,product_id,price,quantity_to_stock)
                 self.__all_products.append(novo_produto)
-                #self.__write()
+                self.__write()
 
     def edit_on_stock(self,product_id,new_product_id,new_product_name,new_price,new_quantity):
         if product_id is not None:
@@ -40,7 +54,7 @@ class ProductManager:
                         items.price = new_price
                     if new_quantity:
                         items.stock = new_quantity
-                    #self.__write()
+                    self.__write()
                     break
             else:
                 print("Produto especificado não existe no banco de dados.")
@@ -50,7 +64,7 @@ class ProductManager:
             for index,items in enumerate(self.__all_products):
                 if items.product_id == product_id:
                     del self.__all_products[index]
-                    #self.__write()
+                    self.__write()
                     break
 
     def get_product(self,product_id):
@@ -64,6 +78,12 @@ class ProductManager:
 
 def testbench():
     print("Rodando Modo Teste!")
+
+    teste_products = ProductManager("./data")
+    teste_products.add_to_stock("123","teste",50,50)
+
+    for testeproduct in teste_products.get_products():
+        print(testeproduct)
 
 if __name__ == "__main__":
     testbench()
