5c367bd34d8ec75922588883639b5721246a6adb
Eduardo S. Brito - archlinux <edusoubrit09@gmail.com>
2026-05-31 03:02:01 -0300

    Adiciona método signin(). Adiciona propriedade personal_session_id. Adiciona atributo personal_permissions.

--- src/controller/appController.py
+++ src/controller/appController.py
@@ -9,19 +9,28 @@ from src.controller.cartController import CartManager
 ########################################################################################################################
 # class
 class AppManager:
-    def __init__(self):
-        self._user_manager = UserManager()
+    def __init__(self,file_path):
+        self._user_manager = UserManager(file_path)
         self._product_manager = ProductManager()
         self._cart_manager = CartManager()
 
         # achar um jeito melhor de criar um admin inicial
-        self._user_manager.create_user("ADMIN", "admin123", "99", "admin@mail", "0", ['admin'])
+        #self._user_manager.create_user("ADMIN", "admin123", "99", "admin@mail", "0", ['admin'])
 
-        self.personal_session_id = None
+        self._personal_session_id = None
+        self.personal_permissions = self._user_manager.get_session_permissions(self.personal_session_id)
+
+    @property
+    def personal_session_id(self):
+        return self._personal_session_id
+    @personal_session_id.setter
+    def personal_session_id(self,value):
+        self._personal_session_id = value
+
     @property
     def is_user_admin(self):
         if self.personal_session_id:
-            return self._user_manager.get_session_id_user(self.personal_session_id).isAdmin()
+            return self._user_manager.get_session_id_user(self.personal_session_id).is_admin()
         else:
             return False
     # --- LOGIN E CADASTRO DE USUARIO ---
@@ -36,13 +45,18 @@ class AppManager:
             return False
 
     def signin(self,data:list):
-        '''
-        data = [nome,senha,cpf,email,contato]
-        '''
-        pass
+        if data:
+            user = self._user_manager.create_user(data[0],data[1],data[2],data[3],data[4],False)
+            if user:
+                return True
+        return False
+
+    def logout(self):
+        self._user_manager.logout(self.personal_session_id)
+        self.personal_session_id = None
+
 
 # testbench
-def testbench():
-    print("Rodando modo teste:")
-if __name__ == '__main__':
-    testbench()
+'''
+testbench somente pelo viewRouter.py
+'''
