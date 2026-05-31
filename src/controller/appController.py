323c4700dfe65a91b079710f061da18c58bf40a5
Eduardo S. Brito - archlinux laptop <edusoubrit09@gmail.com>
2026-05-30 23:08:56 -0300

    Atualiza appController para controle de verificações e interações com o programa.

--- src/controller/appController.py
+++ src/controller/appController.py
@@ -10,119 +10,39 @@ from src.controller.cartController import CartManager
 # class
 class AppManager:
     def __init__(self):
-        self.user_manager = UserManager()
-        self.product_manager = ProductManager()
-        self.cart_manager = CartManager()
+        self._user_manager = UserManager()
+        self._product_manager = ProductManager()
+        self._cart_manager = CartManager()
 
-        self.user_manager.create_user("ADMIN","admin123","adminsupersecretid123","admin@mail","0",['admin'])
+        # achar um jeito melhor de criar um admin inicial
+        self._user_manager.create_user("ADMIN", "admin123", "99", "admin@mail", "0", ['admin'])
 
         self.personal_session_id = None
-
-    def start(self):
-
-        while True:
-            if self.personal_session_id is None:
-                print("Bem vindo!\nO que deseja realizar?\n")
-                print("[ 1 ] - Log-in")
-                print("[ 2 ] - Sign-in")
-                print("[ 3 ] - Sair")
-                opcao = input("Opção: ")
-
-                match opcao:
-                    case '1':
-                        self.login()
-                    case '2':
-                        self.signin()
-                    case '3':
-                        print("Saindo do sistema.")
-                        break
-            else:
-                user = self.user_manager.get_session_id_user(self.personal_session_id)
-                if user.isAdmin():
-                    self.admin_main_menu()
-                else:
-                    self.user_main_menu()
-
-
-
+    @property
+    def is_user_admin(self):
+        if self.personal_session_id:
+            return self._user_manager.get_session_id_user(self.personal_session_id).isAdmin()
+        else:
+            return False
     # --- LOGIN E CADASTRO DE USUARIO ---
-    def login(self):
-        while self.personal_session_id is None:
-            print("--- LOGIN ---")
-            user_id = input("Digite seu CPF (digite 'sair' para sair): ")
+    def login(self,user_id,password):
+        check_session_id = self._user_manager.authenticate_user(user_id,password)
+        if check_session_id:
+            self.personal_session_id = check_session_id
+            print("Usuário logado com exito.")
+            return True
+        else:
+            print("Acesso negado. Tente novamente.")
+            return False
 
-            if user_id.lower() == "sair":
-                print("Saindo do sistema.")
-                break
-
-            user_pswrd = getpass("Digite sua senha: ")
-
-            check_session_id = self.user_manager.authenticate_user(user_id,user_pswrd)
-
-            if check_session_id:
-                self.personal_session_id = check_session_id
-                print("Usuário logado com exito.")
-            else:
-                print("Acesso negado. Tente novamente.")
-
-    def signin(self):
-        pass
-
-    # --- TELA INICIAL ---
-    # -- cliente --
-    def user_main_menu(self):
-        pass
-
-    # -- admin --
-    def admin_main_menu(self):
-        print("--- Painel de Administração ---")
-
-        perms = self.user_manager.get_session_permissions(self.personal_session_id)
-
-        print("[ 1 ] - Ver Catálogo de Produtos")
-        print("[ 2 ] - Ver Relatório Diário")
-        if 'cashier' in perms or 'admin' in perms:
-            print("-- Operações de Caixa --")
-            print("[ 2 ] - Processar Venda Física")
-            print("[ 3 ] - [W.I.P]")
-        if 'admin' in perms:
-            print("-- Gerenciamento de Sistema --")
-            print("[ 4 ] - Gerenciar Clientes")
-            print("[ 5 ] - Gerenciar Estoque")
-
-        opcao = input("Opção: ")
-        match opcao:
-            case '0':
-                print("Fazendo logout.")
-                self.user_manager.logout(self.personal_session_id)
-                self.personal_session_id = None
-                return
-            case '2':
-                pass
-            case '3':
-                pass
-            case '4':
-                if 'admin' in perms:
-                    self.client_manager()
-                else:
-                    print("Opção inválida!")
-            case '5':
-                if 'admin' in perms:
-                    self.stock_manager()
-                else:
-                    print("Opção inválida!")
-            case _:
-                print("Opção inválida!")
-
-    def client_manager(self):
-        pass
-
-    def stock_manager(self):
+    def signin(self,data:list):
+        '''
+        data = [nome,senha,cpf,email,contato]
+        '''
         pass
 
 # testbench
 def testbench():
-    testeApp = AppManager()
-    testeApp.start()
+    print("Rodando modo teste:")
 if __name__ == '__main__':
     testbench()
