eecc561edcab67352ddf493fef1b018b576f0d7c
Eduardo S. Brito - archlinux <edusoubrit09@gmail.com>
2026-05-31 04:48:56 -0300

    Adiciona painel de usuário

--- src/view/userView.py
+++ src/view/userView.py
@@ -1,6 +1,43 @@
 class UserView:
-    def __init__(self,app):
+    def __init__(self,app,storeview_instance):
         self._app = app
+        self._store_view = storeview_instance
 
     def main_menu(self):
-        print("LOGOU")
+        print("--- Painel de Usuário ---")
+
+        print("[ 1 ] - Ver Catálogo de Produtos")
+        print("[ 2 ] - Adicionar ao Carrinho")
+        print("[ 3 ] - Remover do Carrinho")
+        print("[ 4 ] - Finalizar Venda")
+        print("[ 0 ] - Sair")
+
+        opcao = input("Opção: ")
+        match opcao:
+            case '1':
+                self.products_catalog()
+            case '2':
+                self.add_to_cart()
+            case '3':
+                self.remove_from_cart()
+            case '4':
+                self.finish_sale()
+            case '0':
+                return "LOGOUT"
+            case _:
+                print("Opção inválida!")
+
+    def products_catalog(self):
+        self._store_view.list_products()
+
+    def add_to_cart(self):
+        # storeView cuida dessa parte
+        pass
+
+    def remove_from_cart(self):
+        # storeView cuida dessa parte
+        pass
+
+    def finish_sale(self):
+        # storeView cuida dessa parte
+        pass
