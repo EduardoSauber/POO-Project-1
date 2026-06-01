646bafd3bb53905608f2906f0aeaf7e6ef0f61c3
Eduardo S. Brito - archlinux <edusoubrit09@gmail.com>
2026-06-01 00:20:27 -0300

    Adiciona Cadastro de Produto.

--- src/view/storeView.py
+++ src/view/storeView.py
@@ -2,7 +2,90 @@ class StoreView:
     def __init__(self, app):
         self._app = app
 
+    def create_product(self):
+        finalized = False
+        while finalized == False:
+            print("--- CADASTRAR PRODUTO ---")
+            print("Digite 'sair' para cancelar o cadastro.")
+
+            name = None
+            while True:
+                print("Digite o nome do produto:")
+                inpt = input().strip()
+                if inpt.lower() == "sair":
+                    print("Cadastro Cancelado.")
+                    finalized = True
+                    return
+                if inpt:
+                    name = inpt
+                    break
+                else:
+                    print("Entrada inválida! Tente novamente.")
+
+            product_id = None
+            while True:
+                print("Digite o ID do produto (apenas letras e números, sem espaços):")
+                inpt = input().strip()
+                if inpt.lower() == "sair":
+                    print("Cadastro Cancelado.")
+                    finalized = True
+                    return
+                if inpt.isalnum():
+                    product_id = inpt
+                    break
+                else:
+                    print("Entrada inválida! Tente novamente.")
+
+            price = None
+            while True:
+                print("Digite o preço do produto (use '.' para centavos):")
+                inpt = input().strip()
+                if inpt.lower() == "sair":
+                    print("Cadastro Cancelado.")
+                    finalized = True
+                    return
+                if inpt.replace(".", "").isnumeric():
+                    price = float(inpt)
+                    break
+                else:
+                    print("Entrada inválida! Tente novamente.")
+
+            stock = None
+            while True:
+                print("Digite a quantidade inicial de estoque:")
+                inpt = input().strip()
+                if inpt.lower() == "sair":
+                    print("Cadastro Cancelado.")
+                    finalized = True
+                    return
+                if inpt.isnumeric():
+                    stock = int(inpt)
+                    break
+                else:
+                    print("Entrada inválida! Tente novamente.")
+
+            product = self._app.create_product([name, product_id, price, stock])
+            if product:
+                finalized = True
+
     def list_products(self):
         products = self._app.get_all_products()
         for product in products:
             print(product)
+
+    def add_to_user_cart(self):
+        finalized = False
+        while finalized == False:
+            print("--- Adicionar ao Carrinho ---")
+            while True:
+                print("Digite o código do produto (apenas letras e números, sem espaços):")
+                inpt = input("").strip()
+                if inpt.lower() == "sair":
+                    print("Cadastro Cancelado.")
+                    finalized = True
+                    return
+                if inpt.isalnum():
+                    product_id = inpt
+                    break
+                else:
+                    print("Entrada inválida! Tente novamente.")
